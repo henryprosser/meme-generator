40d2723721e73bfc5a9e801b4196062a37a1b559
henryprosser <henryprosser@yahoo.co.uk>
2025-02-04 10:25:23 +0000

    Move hard coded info into state and format code style

--- App.jsx
+++ App.jsx
@@ -1,12 +1,11 @@
-import Header from "./components/Header"
-import Main from "./components/Main"
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
-    
-    return (
-        <>
-            <Header />
-            <Main />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,29 +1,38 @@
-export default function Main() {
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input
-                        type="text"
-                        placeholder="One does not simply"
-                        name="topText"
-                    />
-                </label>
+import { useState } from "react";
 
-                <label>Bottom Text
-                    <input
-                        type="text"
-                        placeholder="Walk into Mordor"
-                        name="bottomText"
-                    />
-                </label>
-                <button>Get a new meme image 🖼</button>
-            </div>
-            <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
-            </div>
-        </main>
-    )
+export default function Main() {
+  /**
+   * Challenge: move the hardcoded meme info into React
+   * state. Use an object with `topText`, `bottomText`,
+   * and `imageUrl` properties, and set the initial values to
+   * the ones hardcoded below.
+   */
+
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input type="text" placeholder="One does not simply" name="topText" />
+        </label>
+
+        <label>
+          Bottom Text
+          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+        </label>
+        <button>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
 }
